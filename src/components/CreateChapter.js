3451eb1b9ab428f828af3408dff7e964ab3b0164
Liz <elizabeth.pinero.r@gmail.com>
2023-05-13 17:20:39 -0400

    Installed TinyMCE Text editor and added to Create Chapter component body input

--- src/components/CreateChapter.js
+++ src/components/CreateChapter.js
@@ -1,6 +1,12 @@
-import React, { useState } from 'react'
+/* eslint-disable max-len */
+/* eslint-disable no-return-assign */
+import React, { useRef, useState } from 'react'
+import { Editor } from '@tinymce/tinymce-react'
 
 export default function CreateChapter() {
+    // Creates reference to tinyMCE editor instance
+    const editorRef = useRef(null);
+
   const [input, setInput] = useState({
     title: null,
     number: null,
@@ -23,10 +29,10 @@ export default function CreateChapter() {
   return (
     <div>
       <form className="flex flex-col">
-        <label htmlFor="title" />
+        <label htmlFor="title">Title</label>
         <input type="text" id="title" onChange={(e) => textOnChange(e, 'title')} />
 
-        <label htmlFor="number" />
+        <label htmlFor="number">Number</label>
         <input
           type="number"
           id="number"
@@ -35,13 +41,33 @@ export default function CreateChapter() {
           onChange={(e) => numberOnChange(e)}
         />
 
-        <label htmlFor="body" />
-        <input type="text" id="body" onChange={(e) => textOnChange(e, 'body')} />
-
-        <label htmlFor="isComplete" />
+        <label htmlFor="body">Body</label>
+        <Editor
+          apiKey={process.env.REACT_APP_TINYMCE_KEY}
+        // Assigns current editor instance to editorRef so contents can be accessed and manipulated programmatically
+        // Without this, the program wouldn't be able to access editor content
+          onInit={(evt, editor) => editorRef.current = editor}
+          className="body"
+          onEditorChange={(e) => textOnChange(e, 'body')}
+          init={{
+            height: 500,
+            menubar: false,
+            plugins: [
+              'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
+              'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
+              'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount',
+            ],
+            toolbar: 'undo redo | blocks | '
+                      + 'bold italic forecolor | alignleft aligncenter '
+                      + 'alignright alignjustify | bullist numlist outdent indent | '
+                      + 'removeformat | help',
+            content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
+          }}
+        />
+        <label htmlFor="isComplete">Is Complete</label>
         <input type="checkbox" id="isComplete" onChange={(e) => checkboxOnChange(e, 'isComplete')} />
 
-        <label htmlFor="isPublished" />
+        <label htmlFor="isPublished">Is Published</label>
         <input type="checkbox" id="isPublished" onChange={(e) => checkboxOnChange(e, 'isPublished')} />
       </form>
     </div>
