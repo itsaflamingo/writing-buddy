623fa32f7b65ce2846f79c663e18364da364c813
Liz <elizabeth.pinero.r@gmail.com>
2023-08-26 14:33:58 -0400

    Can now visit edit chapter page from view chapter page

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,6 +1,31 @@
+import { useRouter } from 'next/router';
 import uniqid from 'uniqid';
+import { useContext } from 'react';
+import { getSelectedDivTitle, getSelectedDoc } from '@/functions/getSelectedDocument';
+import { ChapterContext } from '@/contexts/Contexts';
 
 export default function NavMenu({ context, showMenuItem }) {
+  const router = useRouter();
+  const { chapters } = useContext(ChapterContext);
+
+  const goToEditChapter = (doc, endpoint) => {
+    router.push({
+      pathname: `/chapter/${endpoint}`,
+      query: { data: JSON.stringify(doc) },
+    });
+  }
+
+  const editClickHandler = (e) => {
+    e.stopPropagation();
+
+    const title = getSelectedDivTitle(e);
+    const document = getSelectedDoc(title, chapters);
+
+    goToEditChapter(document, 'create');
+
+    return document;
+  }
+
   return (
     <div className="flex flex-col gap-2 justify-center sticky">
       {context && context.map((data) => (
@@ -9,7 +34,7 @@ export default function NavMenu({ context, showMenuItem }) {
         </button>
       ))}
       <div className="flex flex-col gap-2 m-5">
-        <button className="border-2 px-10" type="button">Edit</button>
+        <button className="border-2 px-10" type="button" onClick={(e) => editClickHandler(e)}>Edit</button>
         <button className="border-2 px-10" type="button">Delete</button>
       </div>
     </div>
