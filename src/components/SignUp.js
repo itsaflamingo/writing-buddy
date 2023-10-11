03957820cb818a9477543baa54cdef0633aa81d8
Liz <elizabeth.pinero.r@gmail.com>
2023-10-11 14:58:38 -0400

    Added background to sign up page

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { useRouter } from 'next/router';
 import { UserContext } from '@/contexts/Contexts';
 import { api_url } from '@/api/url';
+import bgImg from '../images/sign-up-im.jpg'
 
 export default function SignUp() {
   const router = useRouter();
@@ -46,30 +47,46 @@ export default function SignUp() {
       .catch((err) => setError(err))
   }
 
+  const divStyle = {
+    color: 'blue',
+    backgroundImage: `url(${bgImg})`,
+  };
+
   return (
-    <div>
-      <div>New User</div>
-      <form>
-        {error && <div>{error.message}</div>}
-        <label htmlFor="username">Create Username</label>
-        <input
-          id="username"
-          type="text"
-          onChange={(e) => onChangeHandler(e, 'username')}
-        />
-        <label htmlFor="password">Create Password</label>
-        <input
-          id="password"
-          type="password"
-          onChange={(e) => onChangeHandler(e, 'password')}
-        />
-        <button
-          type="submit"
-          onClick={(e) => onSubmit(e)}
-        >
-          Submit
-        </button>
-      </form>
+    <div className="flex">
+      <div className="flex flex-col justify-center border max-w-sm h-screen">
+        <div className="flex justify-center">SIGN UP</div>
+        <div>
+          <form className="flex flex-col m-3">
+            {error && <div>{error.message}</div>}
+            <div className="flex gap-3 m-3 p-3">
+              <label htmlFor="username">Create Username</label>
+              <input
+                className="border"
+                id="username"
+                type="text"
+                onChange={(e) => onChangeHandler(e, 'username')}
+              />
+            </div>
+            <div className="flex gap-3 m-3 p-3">
+              <label htmlFor="password">Create Password</label>
+              <input
+                className="border"
+                id="password"
+                type="password"
+                onChange={(e) => onChangeHandler(e, 'password')}
+              />
+            </div>
+            <button
+              type="submit"
+              onClick={(e) => onSubmit(e)}
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+      <div style={divStyle} className="w-full bg-no-repeat bg-cover" />
     </div>
   )
 }
