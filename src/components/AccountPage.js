0ff4a94a339b78569706d991aa160fc402814f60
Liz <elizabeth.pinero.r@gmail.com>
2024-03-31 17:55:22 -0400

    Added Account settings page, and account and settings pages within, as well as buttons

---
+++ src/components/AccountPage.js
@@ -0,0 +1,12 @@
+export default function AccountPage() {
+  return (
+    <div className="h-screen w-3/4 flex content-start">
+      <button type="button">
+        Update bio
+      </button>
+      <button type="button">
+        Update profile picture
+      </button>
+    </div>
+  )
+}
--- src/pages/account/index.js
+++ src/pages/account/index.js
@@ -1,14 +1,31 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { UserContext } from '@/contexts/Contexts';
+import { useRouter } from 'next/router';
+import Header from '@/components/Header';
+import SettingsNavBar from '@/components/SettingsNavBar';
+import AccountPage from '@/components/AccountPage';
+import Settings from './settings';
 
 export default function Account() {
   const { userData } = useContext(UserContext);
   const { user } = userData;
   const { token } = user;
 
+  const [switchPage, setSwitchPage] = useState(true);
+
   return (
     <div>
-      <h1>Account</h1>
+      <Header />
+      <div className='border-2 border-black flex flex-col h-screen'>
+        <div className='flex justify-center'>
+          <h1>Account</h1>
+        </div>
+        <div className='border-2 border-black h-screen flex'>
+          <SettingsNavBar setPage={setSwitchPage} />
+          {switchPage && <AccountPage />}
+          {!switchPage && <Settings />}
+        </div>
+      </div>
     </div>
   )
 }
